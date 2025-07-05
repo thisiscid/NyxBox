976e226f135926f6d047ddd32705201c849e4b8a
thisisrainy <137782638+thisiscid@users.noreply.github.com>
2025-07-05 01:32:52 +0000

    change gitignore and stuff ya

--- nyxbox/webapp.py
+++ nyxbox/webapp.py
@@ -1,4 +1,5 @@
 from textual_serve.server import Server
 
-server = Server("python -m nyxbox.main --web")
+server = Server("python3.12 -m nyxbox.main --web", public_url="https://nyxbox-client.thisisrainy.hackclub.app", port=26345)
 server.serve()
+
